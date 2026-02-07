9e5f47df0cce16453f112e512129c5a14c9152ea
SirOakenheart <120602835+SirOakenheart@users.noreply.github.com>
2026-02-06 16:23:39 -0800

    Add utils module with math and string functions

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
+from utils import add_numbers, build_message
+
+
 def main():
-    print("Hello, World!")
+    x = 5
+    y = 7
+
+    total = add_numbers(x, y)
+    message = build_message("Edward", total)
+
+    print(message)
+
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,6 @@
+def add_numbers(a, b):
+    return a + b
+
+
+def build_message(name, total):
+    return "Hello " + name + ", the total is " + str(total)
